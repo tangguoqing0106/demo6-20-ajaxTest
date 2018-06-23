bb01a064367f1c34f9b222c0500a5ea1ac3ca721
xiaotangyuan <3192558019@qq.com>
2018-06-23 20:25:27 +0800

    IDEA中的第一次提交

---
+++ src/cn/itszt/Test.java
@@ -0,0 +1,11 @@
+package cn.itszt;
+
+/**
+ * Created by Administrator on 2018\6\23 0023.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(111);
+        System.out.println(222);
+    }
+}
